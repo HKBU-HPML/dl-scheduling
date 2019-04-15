8a3a55a00a53b719f663d46837bae781ba6c5f5c
blackjack201312 <blackjack201303@gmail.com>
2019-04-15 12:18:36 +0800

    finish network contention experiments.

---
+++ network_contention/multiple_osu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+job_num="${job_num:-4}"
+size="${size:-1024}"
+logRoot=logs/job_n${job_num}_s${size}
+mkdir -p $logRoot
+for ((i=1;i<=$job_num;i++))
+do
+    date +"%T.%N"
+    size=$size ./osu_mpi.sh 1>${logRoot}/job_$i.log 2>&1 &
+done
+wait
+sleep 5
---
+++ network_contention/run_benchmark.sh
@@ -0,0 +1,12 @@
+job_num=1 size=1024      sh multiple_osu.sh
+job_num=1 size=4096      sh multiple_osu.sh
+job_num=1 size=16384     sh multiple_osu.sh
+job_num=1 size=65536     sh multiple_osu.sh
+job_num=1 size=262144    sh multiple_osu.sh
+
+job_num=2 size=1024      sh multiple_osu.sh
+job_num=2 size=4096      sh multiple_osu.sh
+job_num=2 size=16384     sh multiple_osu.sh
+job_num=2 size=65536     sh multiple_osu.sh
+job_num=2 size=262144    sh multiple_osu.sh
+
